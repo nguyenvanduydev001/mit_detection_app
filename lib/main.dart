e91fc8e450c311bc8cf9ab5c565dafe6e4670534
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2025-11-21 17:14:39 +0700

    Chỉnh xác nhận email mở được app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:app_links/app_links.dart';
+
 import 'supabase_config.dart';
 import 'pages/login_page.dart';
 import 'pages/register_page.dart';
@@ -10,6 +12,7 @@ import 'pages/about_page.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+
   await Supabase.initialize(
     url: SupabaseConfig.url,
     anonKey: SupabaseConfig.anonKey,
@@ -18,8 +21,31 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final appLinks = AppLinks(); // Listener deep link
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Khi bấm link xác thực email → app tự mở → quay login
+    appLinks.uriLinkStream.listen((uri) {
+      if (uri.toString().contains("account-verified")) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (_) => const LoginPage()),
+          (route) => false,
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +55,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "AGRI VISION",
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF6DBE45)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6DBE45)),
         useMaterial3: true,
         fontFamily: 'Roboto',
       ),
-
-      // Nếu có session → vào Home, không → Login
       home: session == null ? const LoginPage() : HomePage(),
-
       routes: {
         "/login": (_) => const LoginPage(),
         "/register": (_) => const RegisterPage(),
